52bde7fc9467dcc07da105f077d7d9c97cead1f2
Mingfei Gao <g199209@gmail.com>
2018-03-03 23:34:49 +0800

    Update build script

--- build.sh
+++ build.sh
@@ -1,3 +1,17 @@
+echo "Install dependancies? (y/n)?"
+read DEP
+if [ $DEP == "y" ]; then
+sudo apt update
+sudo apt install build-essential libncurses5-dev
+sudo apt install python python3 python-dev python3-dev
+sudo apt install ruby ruby-dev
+sudo apt install lua5.2 liblua5.2-dev luajit libluajit-5.1-dev
+sudo apt install perl libperl-dev
+sudo apt install tcl8.5 tcl8.5-dev libtcl8.5
+sudo apt install exuberant-ctags cscope
+sudo apt install libgnomeui-dev libx11-dev libxt-dev libxpm-dev libgtk2.0-dev libgnome2-dev 
+fi
+
 make clean
 make distclean
 
