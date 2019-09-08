642728e6675ffd20803f269c8d551813a6d10bb2
Gao Mingfei <mingfei.gao@ucloud.cn>
2019-09-08 16:32:59 +0800

    Update set_neovim_env script
    Signed-off-by: Gao Mingfei <mingfei.gao@ucloud.cn>

--- set_neovim_env.sh
+++ set_neovim_env.sh
@@ -5,3 +5,8 @@ ln ~/.vim/.vimrc ~/.vimrc
 rm -rf ~/.config/nvim
 ln -s ~/.vim ~/.config/nvim
 
+pip2 install neovim --upgrade
+pip3 install neovim --upgrade
+
+git config --global core.editor `which nvim`
+
